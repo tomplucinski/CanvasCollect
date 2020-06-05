f7d15e0fa818d27baa08d66df2884a327b37c46c
Tom Plucinski <plucinskitom@yahoo.com>
2020-06-05 12:03:34 -0500

    initial commit

---
+++ CanvasCollectAdmin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CanvasCollectAdmin
+//
+//  Created by Tom Plucinski on 6/5/20.
+//  Copyright © 2020 Tom Plucinski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
