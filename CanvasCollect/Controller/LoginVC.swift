7ab44cb92557d9e8313f24399e6a0d1bfd28851f
Tom Plucinski <plucinskitom@yahoo.com>
2020-06-11 12:20:20 -0500

    Log in existing user

--- CanvasCollect/Controller/LoginVC.swift
+++ CanvasCollect/Controller/LoginVC.swift
@@ -7,24 +7,42 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginVC: UIViewController {
-
+    
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passText: UITextField!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func forgotPassClicked(_ sender: Any) {
     }
-    */
-
+    
+    @IBAction func loginClicked(_ sender: Any) {
+        
+        guard let email = emailText.text, email.isNotEmpty,
+            let password = passText.text, password.isNotEmpty else { return }
+        
+        activityIndicator.startAnimating()
+        Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+            
+            if let error = error {
+                debugPrint(error)
+                self.activityIndicator.stopAnimating()
+                return
+            }
+            
+            self.activityIndicator.stopAnimating()
+            print("Login was successful")
+        }
+    }
+    
+    @IBAction func guestClicked(_ sender: Any) {
+    }
+    
 }
